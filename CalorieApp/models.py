80e8e37f9644c28bafbf6b9d5ff6487f7cea9c79
waweroh <scadmsez@gmail.com>
2023-06-08 20:43:58 +0300

    add models to the calorie app

--- CalorieApp/models.py
+++ CalorieApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Person (models.Model):
+    height = models.IntegerField(max_digits=5)
+    weight = models.IntegerField(max_digits=5)
+
+class Meal (models.Model):
+    name =models.CharField(max_length = 255)
+    date = models.DateField(auto_now_add=True)
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    calories = models.PositiveIntegerField()
+
+class FoodItem (models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
