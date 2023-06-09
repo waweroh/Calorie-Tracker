656e39e01423c976ec07c805678c7c18347b72e9
waweroh <scadmsez@gmail.com>
2023-06-09 13:23:20 +0300

    import models to admin to manipulate through django admin interface

--- CalorieApp/admin.py
+++ CalorieApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Person, Meal, Food, FoodItem
-
+# Importing models into the admin.py file in Django is essential for managing and 
+# manipulating data through the Django admin interface. The admin interface provides
+# a user-friendly way to perform CRUD (Create, Read, Update, Delete) operations on your
+# models and interact with your application's data.
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Meal)
