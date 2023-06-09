5e80e97a95ad6fbf21fe5a1467bbce9b696988ec
waweroh <scadmsez@gmail.com>
2023-06-09 13:10:11 +0300

    add view function connecting to the models created

--- CalorieApp/views.py
+++ CalorieApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import Person, Meal, Food, FoodItem
 
 # Create your views here.
+def person_detail(request, person_id):
+    person = get_object_or_404(Person, id=person_id)
+    return render(request, 'person.html', {'person': person})
+
+def meal_detail(request, meal_id):
+    meal = get_object_or_404(Meal, id=meal_id)
+    food_items = FoodItem.objects.filter(meal=meal)
+    return render(request, 'meal.html', {'meal': meal, 'food_items': food_items})
+
+def food_detail(request, food_id):
+    food = get_object_or_404(Food, id=food_id)
+    return render(request, 'food.html', {'food': food})
