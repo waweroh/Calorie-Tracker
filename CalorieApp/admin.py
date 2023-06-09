22c662191b9479f6de78df991edff00e9b546f04
waweroh <scadmsez@gmail.com>
2023-06-09 09:13:20 +0300

    import and register models in admin file

--- CalorieApp/admin.py
+++ CalorieApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Person, Meal, Food, FoodItem
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Meal)
+admin.site.register(Food)
+admin.site.register(FoodItem)
