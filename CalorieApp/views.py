04bd74906b333ba2b908dcc9b83b790f1636ff20
waweroh <scadmsez@gmail.com>
2023-06-21 17:33:15 +0300

    add id in views

--- CalorieApp/views.py
+++ CalorieApp/views.py
@@ -6,14 +6,14 @@ def index(request):
     if request.method =="POST":
         food_consumed = request.POST.get('food_consumed') #get data/food submitted by user in text not object
         consume = Food.objects.get(name=food_consumed ) #get the food object
-        user = request.user #get the user selecting the food
+        user = request.user.id #get the user selecting the food
         consume = Consumption(user=user, food_consumed=consume)# the actual object
         consume.save()
         foods = Food.objects.all()  
 
     else: 
         foods = Food.objects.all()
-    consumed_foods = Consumption.objects.filter(user=request.user)  # List of consumed food objects to be displayed on template
+    consumed_foods = Consumption.objects.filter(user=request.user.id)  # List of consumed food objects to be displayed on template
     return render(request, 'index.html', {'foods':foods, 'consumed_foods':consumed_foods})
 
 def delete_food(request,id):
