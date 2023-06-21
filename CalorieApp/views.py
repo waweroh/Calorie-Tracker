19f554c2a5fca859d453a3cff70eb03939020ff8
waweroh <scadmsez@gmail.com>
2023-06-21 18:48:00 +0300

    modified user request

--- CalorieApp/views.py
+++ CalorieApp/views.py
@@ -6,8 +6,8 @@ def index(request):
     if request.method =="POST":
         food_consumed = request.POST.get('food_consumed') #get data/food submitted by user in text not object
         consume = Food.objects.get(name=food_consumed ) #get the food object
-        user = request.user.id #get the user selecting the food
-        consume = Consumption(user=user, food_consumed=consume)# the actual object
+        # user = request.user.id #get the user selecting the food
+        consume = Consumption(user=request.user, food_consumed=consume)# the actual object
         consume.save()
         foods = Food.objects.all()  
 
